6d150289d709ca3d7be6bb114570d4a82d6ceb16
Abhishek SIngh <17abhishekk@gmail.com>
2024-07-13 02:42:22 +0530

    minor refactor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 pt-20 md:pt-0"
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
@@ -33,13 +33,9 @@ const Home = () => {
             </Link>
           </div>
         </div>
-
+              
         <div>
-          <img
-            src={HeroImage}
-            alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full"
-          />
+            <img src={HeroImage} alt="mypic" className="rounded-2xl mx-auto w-2/3 md:w-full"/>
         </div>
       </div>
     </div>
